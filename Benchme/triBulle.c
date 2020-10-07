ab78c6fc31f2a1d81b85be2d9e330850a15a65aa
chomette <anthony.chomette@cpe.fr>
2020-10-07 23:00:11 +0200

    premiere version tribulle et triinsertion

--- Benchme/triBulle.c
+++ Benchme/triBulle.c
@@ -17,8 +17,57 @@
 /*
  * 
  */
+int * tribulle(float valeur[],float tailletab)
+{
+    int i,j,tmp;
+ 
+  for (i=0 ; i < tailletab-1; i++)
+  {
+    for (j=0 ; j < tailletab-i-1; j++)
+    {
+      if (valeur[j] > valeur[j+1]) 
+      {
+        tmp = valeur[j];
+        valeur[j] = valeur[j+1];
+        valeur[j+1] = tmp;
+      }
+    }
+  }
+    return valeur;
+}
 int main(int argc, char** argv) {
+    int tailletab = 0,i = 0;
+    for (i=0; i < tailletab; ++i)
+   {
+      
+    float * valeur = NULL;
+    float tri = 0;
+    printf("Combien de valeur avez vous?");
+    scanf("%f", &tailletab);
 
+    if (tailletab > 0) 
+    {
+        valeur = malloc(tailletab * sizeof(int)); 
+        if (valeur == NULL) 
+        {
+            exit(0); 
+        }
+        
+        for (i = 0 ; i < tailletab ; i++)
+        {
+            printf("Quel est la valeur numero %f ? ", i + 1);
+            scanf("%f", &valeur[i]);
+        }
+        tri = tribulle(valeur,tailletab);
+        printf("\n\n Voici les valeurs triées dans l'ordre croissant:\n");
+        for (i = 0 ; i < tailletab ; i++)
+        {
+            printf("%f ans\n", valeur[i]);
+        }
+
+        free(valeur);
+    }
+   }
     return (EXIT_SUCCESS);
 }
 
--- Benchme/triInsertion.c
+++ Benchme/triInsertion.c
@@ -17,8 +17,57 @@
 /*
  * 
  */
-int main(int argc, char** argv) {
+int triinsert(float valeur[],float tailletab)
+{
+    int i,j,tmp;
+    
+    for (i=1; i <= tailletab;i++)
+    {
+        while (j>0 && valeur[j-1])
+        {
+            tmp = valeur[j];
+            valeur[j] = valeur[j-1];
+            valeur[j-1] = tmp;
+            
+            j--;
+        }
+    }
+    return valeur;
+}
 
+int main(int argc, char** argv) {
+float tailletab = 0,i = 0;
+    for (i=0; i < tailletab; ++i)
+   {
+      
+    float* valeur = NULL;
+    float tri = 0;
+    printf("Combien de valeur avez vous?");
+    scanf("%f", &tailletab);
+
+    if (tailletab > 0) 
+    {
+        valeur = malloc(tailletab * sizeof(int)); 
+        if (valeur == NULL) 
+        {
+            exit(0); 
+        }
+        
+        for (i = 0 ; i < tailletab ; i++)
+        {
+            printf("Quel est la valeur numero %f ? ", i + 1);
+            scanf("%f", &valeur[i]);
+        }
+        tri = tribulle(valeur,tailletab);
+        printf("\n\n Voici les valeurs triées dans l'ordre croissant:\n");
+        for (i = 0 ; i < tailletab ; i++)
+        {
+            printf("%f ans\n", valeur[i]);
+        }
+
+        free(valeur);
+    }
+   }
     return (EXIT_SUCCESS);
 }
 
