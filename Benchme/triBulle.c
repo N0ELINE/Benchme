8a8950c25e19399fa27a8d1016ebb23411c39914
N0ELINE <noelinenoeline@gmail.com>
2020-10-08 18:38:07 +0200

    Version finale des tris

--- Benchme/triBulle.c
+++ Benchme/triBulle.c
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -30,5 +31,3 @@ void triBulle(float tab[], int tailletab) {
         }
     }
 }
-
-
--- Benchme/triInsertion.c
+++ Benchme/triInsertion.c
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -35,4 +36,3 @@ void triInsertion(float tab[],int n)
         tab[j]=x;
     }
 }
-
