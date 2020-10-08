eecae25c48e12ef84ad211b9347bcae49f3cb85e
N0ELINE <noelinenoeline@gmail.com>
2020-10-08 18:14:08 +0200

    Main avec csv

--- Benchme/main.c
+++ Benchme/main.c
@@ -18,73 +18,85 @@
 #include "triSelection.h"
 #include "utils.h"
 
-/*
- * @return moyenne des temps dexecution des 3 tests pour le tri par selection
- */
-int testSelec() {
+int main(int argc, char** argv) {
+
+    char chemincsv[500];
+    printf("veuillez entrer le lien du fichier resultat.csv");
+    if (scanf("%254s", chemincsv) != 1) {
+        printf("Erreur lors de la saisie\n");
+        return EXIT_FAILURE;
+    }
+    FILE* fichier = NULL;
+    fichier = fopen(strcat("resultat.csv", chemincsv), "w+");
+    fprintf(fichier, "%s,%f,%f,%f", "TypeTri",100,1000,10000);
+    
+    
     int timeSelec1 = 0;
     float tableauSelection100[100];
-    for (int i=0;i<100;i++){
-        tableauSelection100[i]=randomeur();
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 100; j++) {
+            tableauSelection100[i] = randomeur();
+        }
+        triSelection(tableauSelection100, 100);
     }
-    triSelection(tableauSelection100,100);
-    timeSelec1 = clock();
-    
+    timeSelec1 = clock() / 3;
+
     int timeSelec2 = 0;
     float tableauSelection1000[1000];
-    for (int i=0;i<1000;i++){
-        tableauSelection1000[i]=randomeur();
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 100; j++) {
+            tableauSelection1000[i] = randomeur();
+        }
+        triSelection(tableauSelection1000, 1000);
     }
-    triSelection(tableauSelection1000,1000);
-    timeSelec2 = clock();
-    
+    timeSelec2 = clock() / 3;
+
     int timeSelec3 = 0;
     float tableauSelection10000[10000];
-    for (int i=0;i<10000;i++){
-        tableauSelection10000[i]=randomeur();
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 100; j++) {
+            tableauSelection10000[i] = randomeur();
+        }
+        triSelection(tableauSelection10000, 10000);
     }
-    triSelection(tableauSelection10000,10000);
-    timeSelec3 = clock();
-    
-    float moyenneSelec = (timeSelec1+timeSelec2+timeSelec3)/3;
-    return moyenneSelec;
-}
+    timeSelec3 = clock() / 3;
 
-/*
- * @return moyenne des temps dexecution des 3 tests pour le tri par selection
- */
-int testTas() {
+    fprintf(fichier, "%s,%f,%f,%f", "triSelection",timeSelec1,timeSelec2,timeSelec3);
+
+
+
     int timeTas1 = clock();
     float tableauTas100[100];
-    for (int i=0;i<100;i++){
-        tableauTas100[i]=randomeur();
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 100; j++) {
+            tableauTas100[j] = randomeur();
+        }
+        triParTas(tableauTas100, 100);
     }
-    triParTas(tableauTas100,100);
-    timeTas1= clock();
-    
+    timeTas1 = clock() / 3;
+
     int timeTas2 = clock();
     float tableauTas1000[1000];
-    for (int i=0;i<1000;i++){
-        tableauTas1000[i]=randomeur();
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 1000; j++) {
+            tableauTas1000[j] = randomeur();
+        }
+        triParTas(tableauTas1000, 1000);
     }
-    triParTas(tableauTas1000,1000);
-    timeTas2= clock();
-    
+    timeTas2 = clock() / 3;
+
     int timeTas3 = clock();
     float tableauTas10000[10000];
-    for (int i=0;i<10000;i++){
-        tableauTas10000[i]=randomeur();
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 10000; j++) {
+            tableauTas10000[i] = randomeur();
+        }
+        triParTas(tableauTas10000, 10000);
     }
-    triParTas(tableauTas10000,10000);
-    timeTas3= clock();
-    
-    float moyenneTas = (timeTas1+timeTas2+timeTas3)/3;
-}
+    timeTas3 = clock() / 3;
 
-int main(int argc, char** argv) {
+    fprintf(fichier, "%s,%f,%f,%f", "triTas",timeTas1,timeTas2,timeTas3);
     
-    int tempsSelec = testSelec();
-    int tempsTas = testTas();
     
     return (EXIT_SUCCESS);
 }
